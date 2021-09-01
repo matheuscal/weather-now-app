3240a599c06993d51933366d0e8fb29143d8c235
matheuscal <matheuscalderon@hotmail.com>
2021-09-01 16:31:38 -0400

    minor fix

--- models/mongo.js
+++ models/mongo.js
@@ -1,5 +1,5 @@
 const mongo = require('mongoose');
-mongo.connect('mongodb+srv://lancelot:netgate@cluster0.xxxgs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+mongo.connect(process.env.MONGO_URI || 'mongodb+srv://lancelot:netgate@cluster0.xxxgs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then((data) => {
         console.log('Database: Connected.');
     })
